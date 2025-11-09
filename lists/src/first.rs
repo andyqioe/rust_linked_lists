cb16e4ba42ea649538d04e455f94ee85d2d2457f
Andy Qiu <andyqiu40@gmail.com>
2025-11-08 18:50:18 -0800

    done with singly list

--- lists/src/first.rs
+++ lists/src/first.rs
@@ -1,3 +1,5 @@
+use std::mem;
+
 pub struct List {
     head: Link
 }
@@ -13,11 +15,54 @@ enum Link {
 }
 
 impl List {
-    pub fn new() -> self {
-        // Initializes a 0 init list data struct
+    pub fn new() -> Self {
         List { head: Link::Empty }
     }
-    fn append(&mut self, link: Link) {}
-    fn pop(&mut self) -> Link {}
-    fn find(&self, val: i32) -> bool {}
+    fn push(&mut self, elem: i32) {
+        let boxed: Box<Node> = Box::new(
+            Node { elem: elem, next: mem::replace(&mut self.head, Link::Empty) }
+        );
+        self.head = Link::More(boxed);
+    }
+    fn pop(&mut self) -> Option<i32> {
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => None,
+            Link::More(node) => {
+                self.head = node.next;
+                Some(node.elem)
+            },
+        }
+    }
+    // fn find(&self, val: i32) -> bool {}
+}
+
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut link = mem::replace(&mut self.head, Link::Empty);
+        while let Link::More(mut node) = link {
+            link = mem::replace(&mut node.next, Link::Empty);
+        }
+    }
+}
+#[cfg(test)]
+mod test {
+    use super::List;
+    #[test] 
+    fn basics() {
+        let mut list = List::new();
+        assert_eq!(list.pop(), None);
+
+        list.push(1);
+        list.push(2332423);
+        list.push(2222);
+
+        let popped = list.pop();
+        assert_eq!(popped, Some(2222));
+        let popped = list.pop();
+        assert_eq!(popped, Some(2332423));
+        let popped = list.pop();
+        assert_eq!(popped, Some(1));
+        let popped = list.pop();
+        assert_eq!(popped, None);
+    }
 }
---
+++ lists/target/debug/deps/lists-459f96d05f9d6247.d
@@ -0,0 +1,6 @@
+C:\Users\andyq\Github\rust_linked_lists\lists\target\debug\deps\lists-459f96d05f9d6247.d: src\lib.rs src\first.rs
+
+C:\Users\andyq\Github\rust_linked_lists\lists\target\debug\deps\lists-459f96d05f9d6247.exe: src\lib.rs src\first.rs
+
+src\lib.rs:
+src\first.rs:
